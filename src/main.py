77a4b8b3745408ab63a1ec1218bc6b5f80fe0681
lorber190103 <lorber190103@sr.htlweiz.at>
2024-01-16 12:43:22 +0100

    Works

--- src/main.py
+++ src/main.py
@@ -5,18 +5,21 @@ import sys
 
 class JsonConverter:
     def __init__(self):
-        self.data = {}
+        self.data = {"Buch": []}
 
     def read_csv(self, csvFilePath: str):
         with open(f"data/{csvFilePath}", encoding='utf-8') as csv_file:
             csvReader = csv.DictReader(csv_file)
             for rows in csvReader:
-                key = rows['Titel']
-                self.data[key] = rows
+                self.data["Book"].append(rows)
+
+    def clean_whitespace(self, dictionary):
+        return {key: value.strip() if isinstance(value, str) else value for key, value in dictionary.items()}
 
     def make_json(self, jsonFilePath: str):
+        cleaned_data = {"Book": [self.clean_whitespace(book) for book in self.data["Book"]]}
         with open(f"data/{jsonFilePath}", 'w', encoding='utf-8') as json_file:
-            json_file.write(json.dumps(self.data, indent=4))
+            json_file.write(json.dumps(cleaned_data, indent=4))
 
 
 def main():
